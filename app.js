1de176db61ec6c1bca58a110914b8163b642f9b8
hk265 <Nimcet9900@gmail.com>
2025-02-03 18:24:13 +0530

    final commit

--- app.js
+++ app.js
@@ -1,36 +1,90 @@
 
+
+function maskPassword(pass){
+    let str = "";
+    for (let index = 0; index < pass.length; index++) {
+        str += "*";
+     
+        
+    }
+    return str
+}
+
+function copyText(txt){
+    navigator.clipboard.writeText(txt).then(
+        ()=>{
+            // alert("Copied the text:"+txt);
+            document.getElementById("alert").style.display = "inline";
+            setTimeout(() => {
+                document.getElementById("alert").style.display = "none";
+
+                
+            }, 2000);
+        },
+        ()=>{
+            alert("failed");
+        },
+
+    );
+}
+ 
+
 // Logic to fill the table
 
-let tb = document.querySelector("table")
-let data = localStorage.getItem("passwords")
+const deletePassword = (website)=>{
+    let data = localStorage.getItem("passwords");
+    let arr = JSON.parse(data);
+    let arrUpdated = arr.filter((e)=>{
+        return e.website != website
+    })
+    localStorage.setItem("passwords",JSON.stringify(arrUpdated));
+    alert(`Successfully deleted ${website}'s password`);
+    showPasswords();
+
+
+}
+const showPasswords = () =>{
+let tb = document.querySelector("table");
+let data = localStorage.getItem("passwords");
 
 if(data == null){
     tb.innerHTML = "No Data To Show";
 }
 else{
 
+    tb.innerHTML = `<tr>
+    <th>Website</th>
+    <th>Username</th>
+    <th>Password</th>
+    <th>Delete</th>
+</tr>`
+
 let arr = JSON.parse(data);
 let str = "";
-for (let index = 0; index < array.length; index++) {
+for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
     
 
 
-str = ` <tr>
-<td>${element.Website}</td>
-<td>${element.Username}</td>
-<td>${element.Password}</td>
-<td>${"Delete"}</td>
+str += ` <tr>
+<td>${element.website}<img onclick="copyText('${element.website}')" src ="copy.svg" alt="Copy Button" width="10" width="10" height="10"></td>
+<td>${element.username}<img onclick="copyText('${element.username}')" src ="copy.svg" alt="Copy Button" width="10" width="10" height="10"></td>
+<td>${maskPassword(element.password)}<img onclick="copyText('${element.password}')" src ="copy.svg" alt="Copy Button" width="10" width="10" height="10"></td>
+<td><button class ="btnsm" onclick="deletePassword('${element.website}')">Delete<button></td>
 </tr>
 `
 
 }
-tb.innerHTML = tb.innerHTML + str
+tb.innerHTML += str;
 }
+}
+
+showPasswords();
 
 // ....................my code .........................
 
 let btn = document.querySelector(".btn");
+let website = document.querySelector("#website");
 let username = document.querySelector("#username");
 let password = document.querySelector("#password");
 btn.addEventListener("click",(e)=>{
@@ -41,18 +95,20 @@ btn.addEventListener("click",(e)=>{
     console.log(passwords);
     if(passwords == null){
         let json = [];
-        json.push({username:username.value,password:password.value});
+        json.push({website:website.value,username:username.value,password:password.value});
         alert("Password saved");
         localStorage.setItem("passwords",JSON.stringify(json));
     }
 
     else{
         let json = JSON.parse(localStorage.getItem("passwords"));
-        json.push({username:username.value,password:password.value});
+        json.push({website:website.value, username:username.value,password:password.value});
         alert("Password saved");
         localStorage.setItem("passwords",JSON.stringify(json));
         
     }
+
+    showPasswords();
 
 })
 
